c5412ecce2318520e634534c7f6b0c1e6c488195
Weeravit Sirita <weeravit.sirita@gmail.com>
2017-02-07 22:58:13 +0700

    Add automate ui testing

--- app/src/main/java/me/weeravit/androidnewssample/module/news/adapter/NewsAdapter.java
+++ app/src/main/java/me/weeravit/androidnewssample/module/news/adapter/NewsAdapter.java
@@ -1,6 +1,5 @@
 package me.weeravit.androidnewssample.module.news.adapter;
 
-import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
